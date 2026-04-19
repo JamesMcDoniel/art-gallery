d5424ee5daddfb4aefefb5a45eae6c6fbe4b8ef3
JamesMcDoniel <87280363+JamesMcDoniel@users.noreply.github.com>
2026-04-19 09:57:59 -0500

    Fix width and scroll issues

--- src/components/Layout/components/Content/index.jsx
+++ src/components/Layout/components/Content/index.jsx
@@ -1,7 +1,23 @@
+import { useRef, useEffect } from 'react';
+import { useLocation } from 'react-router';
 import styles from './Content.module.css';
 
 const Content = ({ children }) => {
-    return <main className={styles.container}>{children}</main>;
+    const containerRef = useRef();
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        containerRef.current?.scrollTo(0, 0);
+    }, [pathname]);
+
+    return (
+        <main
+            className={styles.container}
+            ref={containerRef}
+        >
+            {children}
+        </main>
+    );
 };
 
 export default Content;
